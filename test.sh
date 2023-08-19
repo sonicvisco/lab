628bbcc6a53c7e4918bc55b845d4dc6332d1e1d0
sonicvisco <139844217+sonicvisco@users.noreply.github.com>
2023-08-19 10:14:44 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ gsutil cp usa_names.csv gs://$PROJECT_ID
 curl https://cdn.qwiklabs.com/8tnHNHkj30vDqnzokQ%2FcKrxmOLoxgfaswd9nuZkEjd8%3D --output head_usa_names.csv
 gsutil cp head_usa_names.csv gs://$PROJECT_ID
 bq mk lake
-bq load --autodetect --source_format=CSV lake.usa_names gs://qwiklabs-gcp-00-5abf2e2e4bec/csv
-bq load --autodetect --source_format=CSV lake.usa_names_transformed gs://qwiklabs-gcp-00-5abf2e2e4bec/csv
-bq load --autodetect --source_format=CSV lake.usa_names_enriched gs://qwiklabs-gcp-00-5abf2e2e4bec/csv
-bq load --autodetect --source_format=CSV lake.orders_denormalized_sideinput gs://qwiklabs-gcp-00-5abf2e2e4bec/csv
+bq load --autodetect --source_format=CSV lake.usa_names gs://qwiklabs-gcp-03-fdfcc139c9e4/csv
+bq load --autodetect --source_format=CSV lake.usa_names_transformed gs://qwiklabs-gcp-03-fdfcc139c9e4/csv
+bq load --autodetect --source_format=CSV lake.usa_names_enriched gs://qwiklabs-gcp-03-fdfcc139c9e4/csv
+bq load --autodetect --source_format=CSV lake.orders_denormalized_sideinput gs://qwiklabs-gcp-03-fdfcc139c9e4/csv
